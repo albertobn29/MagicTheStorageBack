f87a4e4bf1f2c2954f795ed26379f2b9bc79b675
albertobn29 <albertobenitezn@gmail.com>
2022-02-19 18:01:44 +0100

    cors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const app = express();
 const { sequelize } = require('./models/index');
-const cors = require('cors');
+var cors = require('cors');
 const routes = require('./routes');
-
 const PORT = process.env.PORT || 3500;
 
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 
 app.use('/api', routes);
 
